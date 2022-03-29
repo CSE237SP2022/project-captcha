045372e66a855b3081fcd5c0bba89fbda5e87bb8
sichi-o <54566593+sichi-o@users.noreply.github.com>
2022-03-28 20:35:32 -0500

    now works!! (*kinda)

--- src/captcha/Prompt.java
+++ src/captcha/Prompt.java
@@ -3,20 +3,6 @@ package captcha;
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-//import java.awt.TextField;
-import javax.swing.event.DocumentListener;
-
-import java.lang.Object;
-import java.awt.Graphics;
 
 public class Prompt {
 
@@ -24,6 +10,10 @@ public class Prompt {
 
 	public Prompt() {
 		this.answer = "This is a CAPTcha";
+	}
+	
+	public String getAnswer() {
+		return answer;
 	}
 
 	public void randomize() {
@@ -39,53 +29,16 @@ public class Prompt {
 		 */
 
 	}
-
-	
-	public void paint(Graphics g)
-	{	
-	   g.drawString("abc", 30, 180);
-	   g.dispose();
-	}
 	
 	public void render() {
 		// randomize();
-
-		// Got the idea for using JFrame from this website:
-		// https://www.tutorialspoint.com/what-is-the-use-of-setbounds-method-in-java
-		JFrame frame = new JFrame("SetBounds Method Test");
-		frame.setSize(400, 400);
-		
-		frame.setLayout(null);
-		
-		JTextField text = new JTextField("hello");
-		JButton button = new JButton("hello");
-		JLabel captcha = new JLabel(answer);
-
-		text.setBounds(80, 90, 120, 40);
-		button.setBounds(80, 150, 120, 40);
-		captcha.setBounds(80, 30, 120, 40);
-
-		frame.add(captcha);
-		frame.add(text);
-
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String userInput = text.getText();
-				System.out.println(userInput);
-			}
-		});
-
-		frame.add(button);
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+	    StdDraw.setPenColor(Color.BLACK);
+	    StdDraw.text(0.2, 0.5, answer);
 
 	}
 
 	public static void main(String[] args) {
 		Prompt newprompt = new Prompt();
-		
 		newprompt.render();
 	}
 
