8755bd6e8d0dc1378ef4ccb7435faee63b717252
Quan Khuc <71282105+quankhuc@users.noreply.github.com>
2022-04-12 01:21:29 -0500

    Build the distort for the captcha text (#15)
    * Update Prompt.java
    
    * Randomization functionality implemented
    
    Co-authored-by: John Spencer <J.s.spencer@wustl.edu>

--- src/captcha/Prompt.java
+++ src/captcha/Prompt.java
@@ -1,7 +1,8 @@
 package captcha;
 
 import edu.princeton.cs.introcs.StdDraw;
-
+import java.awt.Font;
+import java.util.ArrayList;
 import java.awt.*;
 
 public class Prompt {
@@ -19,6 +20,7 @@ public class Prompt {
 	public static void randomize() {
 
 		String word_bank = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		
 		
 		int answer_length = (int) (Math.random() * (12 - 6)) + 6;
 		
@@ -40,9 +42,21 @@ public class Prompt {
 
 	public void render() {
 		randomize();
+		Font myFont = new Font("TimesNewRoman", Font.BOLD, 20); 
 		StdDraw.clear();
 		StdDraw.setPenColor(Color.BLACK);
-		StdDraw.text(0.2, 0.8, answer);
+		StdDraw.setFont(myFont);
+		System.out.println(answer); 
+		SpecialCharacters myChars = new SpecialCharacters(answer.length()); 
+		ArrayList<Font> myArray = myChars.randomizer(); 
+		for(int i = 0; i < myArray.size(); ++i) {
+			StdDraw.setFont(myArray.get(i));
+			StdDraw.text(0.2 + 0.05*i, 0.8, Character.toString(answer.charAt(i)));
+		}
+		// StdDraw.text(0.2, 0.8, Character.toString(answer.charAt(0)));
+//		StdDraw.text(0.2, 0.8, answer); 
+		System.out.println(answer); 
 
 	}
+	
 }
