b31f3da572186897ae49f196b4906dbacd4df0fe
Spencej1 <J.s.spencer@wustl.edu>
2022-04-25 23:47:04 -0500

    Draw Shapes implemented

--- src/captcha/DrawShapes.java
+++ src/captcha/DrawShapes.java
@@ -1,48 +1,71 @@
 package captcha;
 
+import java.awt.Color;
 import java.awt.Font;
+import edu.princeton.cs.introcs.StdDraw;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class DrawShapes {
-	double x; // x coordinate of the shape
-	double y; // y coordinate of the shape
-	double radius; // radius of the circle
-	double halfLength; // one half the length of any side of the square
-	double semiMajorAxis; // is the semimajor axis of the ellipse
-	double semiMinorAxis; // is the semiminor axis of the ellipse
+	private double x; // x coordinate of the shape
+	private double y; // y coordinate of the shape
+	private double radius; // radius of the circle
+	private double halfLength; // one half the length of any side of the square
+	private double semiMajorAxis; // is the semimajor axis of the ellipse
+	private double semiMinorAxis; // is the semiminor axis of the ellipse
 
-	int smallLowerBound = 0;
-	int smallUpperBound = 1;
-	int mediumLowerBound = 2;
-	int mediumUpperBound = 3;
-	int largeLowerBound = 4;
-	int largeUpperBound = 5;
+	private double smallLowerBound;
+	private double smallUpperBound;
+	private double mediumLowerBound;
+	private double mediumUpperBound;
+	private double largeLowerBound;
+	private double largeUpperBound;
 
-	int lowerBoundOfScreenX = 0; // Fill in these values
-	int upperBoundOfScreenX = 0; // Fill in these values
+	private double lowerBoundOfScreenX; // Fill in these values
+	private double upperBoundOfScreenX; // Fill in these values
 
-	int lowerBoundOfScreenY = 0; // Fill in these values
-	int uppserBoundOfScreenY = 0; // Fill in these values
+	private double lowerBoundOfScreenY; // Fill in these values
+	private double upperBoundOfScreenY; // Fill in these values
+	
+	private Color[] myColors=new Color[8];
 
-	private enum Colors {
-		BookLightBlue, Orange, Pink, Yellow
+	public DrawShapes() {
+		this.x = 0; 
+		this.y = 0; 
+		this.radius = 0; 
+		this.halfLength =0; 
+		this.semiMajorAxis = 0; 
+		this.semiMinorAxis = 0; 
+		
+		this.smallLowerBound = 0; 
+		this.smallUpperBound = 0.1; 
+		this.mediumLowerBound = 0.2; 
+		this.mediumUpperBound = 0.3; 
+		this.largeLowerBound = 0.4; 
+		this.largeUpperBound = 0.5; 
+		
+		this.lowerBoundOfScreenX = 0;
+		this.upperBoundOfScreenX = 1; 
+		
+		this.lowerBoundOfScreenY = 0; 
+		this.upperBoundOfScreenY = 1; 	
+		
+		this.myColors[0]=Color.CYAN;
+		this.myColors[1]=Color.BLUE;
+		this.myColors[2]=Color.ORANGE;
+		this.myColors[3]=Color.RED;
+		this.myColors[4]=Color.GREEN;
+		this.myColors[5]=Color.YELLOW;
+		this.myColors[6]=Color.PINK;
+		this.myColors[7]=Color.GREEN;
+		
+		StdDraw.setCanvasSize(512, 512); 
+
 	}
+	
 
 	private enum Sizes {
 		Small, Medium, Large
-	}
-
-	private String assignColorValue(Colors c) {
-		if (c == Colors.BookLightBlue) {
-			return "BOOK_LIGHT_BLUE";
-		} else if (c == Colors.Orange) {
-			return "ORANGE";
-		} else if (c == Colors.Pink) {
-			return "PINK";
-		} else {
-			return "YELLOW";
-		}
 	}
 
 	private double assignShapeSize(Sizes s) {
@@ -54,9 +77,10 @@ public class DrawShapes {
 			return setSizeOfShape(largeLowerBound, largeUpperBound); // play around with these bounds
 		}
 	}
-
-	public Colors setRandomColor() { // This method selects a random color from the Colors enumeration
-		return Colors.values()[new Random().nextInt(Colors.values().length)];
+	
+	private Color getRandomColor() {
+		int randomIdx = (int) (Math.random()*7); 
+		return myColors[randomIdx]; 
 	}
 
 	public Sizes setRandomSize() { // This method selects a random size from the Sizes enumeration
@@ -64,7 +88,7 @@ public class DrawShapes {
 	}
 
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-	public double setSizeOfShape(int lowerBound, int upperBound) { // This method sets the size of the shape given an
+	public double setSizeOfShape(double lowerBound, double upperBound) { // This method sets the size of the shape given an
 																	// upper and lower bound, which is unique for each
 																	// Sizes enumeration
 		return Math.random() * (upperBound - lowerBound) + lowerBound;
@@ -72,7 +96,7 @@ public class DrawShapes {
 
 	// should there be a method to assign the x and y coordinate, so they are not
 	// too far from one another and take up the whole screen?
-	public double setXLocation(int lowerBound, int upperBound) { // This method sets the x coordinate of a shape, given
+	public double setXLocation(double lowerBound, double upperBound) { // This method sets the x coordinate of a shape, given
 																	// the bounds of screen
 		return Math.random() * (upperBound - lowerBound) + lowerBound;
 	}
@@ -85,23 +109,24 @@ public class DrawShapes {
 		return x + randomChoice;
 	}
 
-	private void render() {
+	public void render() {
 		int count = 0;
-		while (count < 10) {
+		while (count < 100) {
 			double rCircle = setXLocation(lowerBoundOfScreenX, upperBoundOfScreenX);
-			StdDraw.setColor(assignColorValue(setRandomColor()));
+			StdDraw.setPenColor(getRandomColor());
 			StdDraw.filledCircle(rCircle, setYLocation(rCircle), assignShapeSize(setRandomSize()));
 
 			double rSquare = setXLocation(lowerBoundOfScreenX, upperBoundOfScreenX);
-			StdDraw.setColor(assignColorValue(setRandomColor()));
+			StdDraw.setPenColor(getRandomColor());
 			StdDraw.filledSquare(rSquare, setYLocation(rSquare), assignShapeSize(setRandomSize()));
 
 			double rElipse = setXLocation(lowerBoundOfScreenX, upperBoundOfScreenX);
-			StdDraw.setColor(assignColorValue(setRandomColor()));
+			StdDraw.setPenColor(getRandomColor());
 			StdDraw.filledEllipse(rElipse, setYLocation(rElipse), assignShapeSize(setRandomSize()), assignShapeSize(setRandomSize()));
 
 			++count;
 		}
+		
 
 	}
 
